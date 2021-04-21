d5686f3c66f2c7747443697a485ae6c8ffdbc5a6
Pethuvignesh K <vignesh@isquarebs.com>
2021-04-21 11:42:46 +0530

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
-
+import { Columns, Config, DefaultConfig } from 'ngx-easy-table';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Easy-Table-Angular';
+  public configuration: Config;
+  public columns: Columns[];
+
+  public data = [{
+    phone: '+91 9698631958',
+    age: 26,
+    address: { street: 'North street', number: 12 },
+    company: 'iSquareBS',
+    name: 'Vignesh K',
+    isActive: true,
+  }, {
+    phone: '+91 7904028760',
+    age: 25,
+    address: { street: 'South street', number: 12 },
+    company: 'iSquare',
+    name: 'Viki K',
+    isActive: true,
+  }, {
+    phone: '+91 7904028760',
+    age: 31,
+    address: { street: 'South street', number: 12 },
+    company: 'KNOWLYSIS',
+    name: 'Heidi Duncan',
+    isActive: true,
+  }, {
+    phone: '+91 7904028760',
+    age: 21,
+    address: { street: 'South street', number: 12 },
+    company: 'KNOWLYSIS',
+    name: 'Duncan',
+    isActive: true,
+  }, {
+    phone: '+91 7502357602',
+    age: 24,
+    address: { street: 'South street', number: 12 },
+    company: 'KNOWLYSIS',
+    name: 'Bheem',
+    isActive: true,
+  }, {
+    phone: '+91 9566598047',
+    age: 29,
+    address: { street: 'South street', number: 12 },
+    company: 'KNOWLYSIS',
+    name: 'Bheem',
+    isActive: true,
+  }
+
+];
+
+  ngOnInit(): void {
+    this.configuration = { ...DefaultConfig };
+    this.configuration.searchEnabled = true;
+    // ... etc.
+    this.columns = [
+      { key: 'phone', title: 'Phone' },
+      { key: 'age', title: 'Age' },
+      { key: 'company', title: 'Company' },
+      { key: 'name', title: 'Name' },
+      { key: 'isActive', title: 'STATUS' },
+    ];
+  }
 }
